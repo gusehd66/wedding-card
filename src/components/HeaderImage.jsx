6b71c73a1735d110a391447dee13f02fd2e00b84
khd226 <khd226@hfrnet.com>
2026-02-07 13:13:21 +0900

    effect

--- src/components/HeaderImage.jsx
+++ src/components/HeaderImage.jsx
@@ -2,6 +2,7 @@ export default function HeaderImage() {
   return (
     <div className="header-image-wrapper">
       <img src="/images/header_image.jpg" alt="메인 이미지" className="header-image" loading="eager" />
+      <img src="/images/effect_cherryblossom.png" alt="벚꽃 효과" className="header-effect-cherryblossom" loading="eager" />
     </div>
   )
 }
